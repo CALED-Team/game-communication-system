488952de36b046b6f4b38fef85d4d0202ab18e23
Amir Toosi <amir.ghalb@gmail.com>
2023-03-29 19:58:06 +1100

    modified debug_run.sh to be more generic and take arguments for the image names

--- bin/debug_run.sh
+++ bin/debug_run.sh
@@ -3,21 +3,29 @@
 # Creates a test image based on the server_base dockerfile and runs the server + two clients with that image.
 
 # shellcheck disable=SC2164
-BASE_DIR="$( dirname "$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" )"
-BIN_DIR="${BASE_DIR}/bin"
-SRC_DIR="${BASE_DIR}/src"
-DOCKERFILES_DIR="${BASE_DIR}/dockerfiles"
+GCS_DIR="$( dirname "$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" )"
+BIN_DIR="${GCS_DIR}/bin"
+SRC_DIR="${GCS_DIR}/src"
+DOCKERFILES_DIR="${GCS_DIR}/dockerfiles"
+BASE_DIR="$(dirname $GCS_DIR)"
 
-TARGET_IMAGE="game-server-23"
-CLIENT_TARGET_IMAGE="game-client-23"
+TARGET_IMAGE=$1
+SERVER_DIR="$BASE_DIR/$TARGET_IMAGE"
 
-cp "${DOCKERFILES_DIR}/server_base" "$(dirname $BASE_DIR)/game-server-23/Dockerfile"
-cd "$BIN_DIR" || exit
-docker build $(dirname $BASE_DIR)/game-server-23 -t "$TARGET_IMAGE"
+CLIENT1_TARGET_IMAGE=$2
+CLIENT1_DIR="$BASE_DIR/$CLIENT1_TARGET_IMAGE"
 
-cp "${DOCKERFILES_DIR}/client_base" "$(dirname $BASE_DIR)/game-client-23/Dockerfile"
-cd "$BIN_DIR" || exit
-docker build $(dirname $BASE_DIR)/game-client-23 -t "$CLIENT_TARGET_IMAGE"
+CLIENT2_TARGET_IMAGE=$2
+CLIENT2_DIR="$BASE_DIR/$CLIENT2_TARGET_IMAGE"
+
+cp "${DOCKERFILES_DIR}/server_base" "$SERVER_DIR/Dockerfile"
+docker build $SERVER_DIR -t "$TARGET_IMAGE"
+
+cp "${DOCKERFILES_DIR}/client_base" "$CLIENT1_DIR/Dockerfile"
+docker build $CLIENT1_DIR -t "$CLIENT1_TARGET_IMAGE"
+
+cp "${DOCKERFILES_DIR}/client_base" "$CLIENT2_DIR/Dockerfile"
+docker build $CLIENT2_DIR -t "$CLIENT2_TARGET_IMAGE"
 
 cd "$SRC_DIR" || exit
 cat <<EOF > _temp_clients_file.json
@@ -25,12 +33,12 @@ cat <<EOF > _temp_clients_file.json
   {
     "id": "client1",
     "name": "Client One!",
-    "image": "$CLIENT_TARGET_IMAGE"
+    "image": "$CLIENT1_TARGET_IMAGE"
   },
   {
     "id": "client2",
     "name": "Client Two!",
-    "image": "$CLIENT_TARGET_IMAGE"
+    "image": "$CLIENT2_TARGET_IMAGE"
   }
 ]
 EOF
